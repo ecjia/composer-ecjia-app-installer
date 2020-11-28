a9aea33331820de85e776f5e1fc8a84fb90e19b8
王正东 <wangzhengdong@ecmoban.com>
2020-11-28 16:52:57 +0800

    更新模板

--- templates/front/deploy.dwt.php
+++ templates/front/deploy.dwt.php
@@ -12,41 +12,41 @@
                         <h4>{t domain="installer"}数据库配置{/t}</h4>
                         <ul class="ecjia-option">
 						    <li class="control-group">
-						        <label class="ecjia-label" for="dbhost">{t domain="installer"}数据库主机{/t}</label>
-						        <input type="text" class="text" name="dbhost" id="dbhost" value="localhost"/>
+						        <label class="ecjia-label" for="db_host">{t domain="installer"}数据库主机{/t}</label>
+						        <input type="text" class="text" name="db_host" id="db_host" value="localhost"/>
 						        <p class="description">{t domain="installer" 1="localhost"}您可能会使用"%1"{/t}</p>
 						    </li>
 						    
 						    <li class="control-group">
-						        <label class="ecjia-label" for="dbport">{t domain="installer"}数据库端口{/t}</label>
-						        <input type="text" class="text" name="dbport" id="dbport" value="3306"/>
+						        <label class="ecjia-label" for="db_port">{t domain="installer"}数据库端口{/t}</label>
+						        <input type="text" class="text" name="db_port" id="db_port" value="3306"/>
 						        <p class="description">{t domain="installer"}如果您不知道此选项的意义， 请保留默认设置{/t}</p>
 						    </li>
 						    
 						    <li class="control-group">
-						        <label class="ecjia-label" for="dbuser">{t domain="installer"}数据库用户名{/t}</label>
-						        <input type="text" class="text" name="dbuser" id="dbuser" value="root" />
+						        <label class="ecjia-label" for="db_user">{t domain="installer"}数据库用户名{/t}</label>
+						        <input type="text" class="text" name="db_user" id="db_user" value="root" />
 						        <p class="description">{t domain="installer" 1="root"}您可能会使用 "%1"{/t}</p>
 						    </li>
 						    
 						    <li class="control-group">
 						    	<div class="check_db_correct" data-url="{RC_Uri::url('installer/index/check_db_correct')}">
-							        <label class="ecjia-label" for="dbpassword">{t domain="installer"}数据库密码{/t}</label>
-							        <input type="password" class="text" name="dbpassword" id="dbpassword" value="" />
+							        <label class="ecjia-label" for="db_password">{t domain="installer"}数据库密码{/t}</label>
+							        <input type="password" class="text" name="db_password" id="db_password" value="" />
 						        </div>
 						    </li>
 						    
 						    <li class="control-group">
                                 <div class="check_db_exists" data-url="{RC_Uri::url('installer/index/check_db_exists')}">
-                                    <label class="ecjia-label" for="dbpassword">{t domain="installer"}数据库名称{/t}</label>
-                                    <input type="text" class="text" name="dbdatabase" id="dbdatabase" value="" />
+                                    <label class="ecjia-label" for="db_database">{t domain="installer"}数据库名称{/t}</label>
+                                    <input type="text" class="text" name="db_database" id="db_database" value="" />
                                 </div>
                                 <p class="description">{t domain="installer"}请您指定数据库名称{/t}</p>
                             </li>
 						    
                             <li class="control-group">
-	                            <label class="ecjia-label" for="dbprefix">{t domain="installer"}数据库前缀{/t}</label>
-	                            <input type="text" class="text" name="dbprefix" id="dbprefix" value="ecjia_" />
+	                            <label class="ecjia-label" for="db_prefix">{t domain="installer"}数据库前缀{/t}</label>
+	                            <input type="text" class="text" name="db_prefix" id="db_prefix" value="ecjia_" />
 	                            <p class="description">{t domain="installer" 1="ecjia_"}默认前缀是 "%1"{/t}</p>
                             </li>
                         </ul>
@@ -60,20 +60,20 @@
                             </li>
                             
                             <li class="control-group">
-	                            <label class="ecjia-label" for="userpassword">{t domain="installer"}登录密码{/t}</label>
-	                            <input type="password" name="userpassword" id="userpassword" class="text" value="" />
+	                            <label class="ecjia-label" for="user_password">{t domain="installer"}登录密码{/t}</label>
+	                            <input type="password" name="user_password" id="user_password" class="text" value="" />
 	                            <p class="password-error"></p>
                             </li>
                             
                             <li class="control-group">
-	                            <label class="ecjia-label" for="confirmpassword">{t domain="installer"}确认密码{/t}</label>
-	                            <input type="password" name="confirmpassword" id="confirmpassword" class="text" value="" />
-	                            <p class="confirmpassword-error"></p>
+	                            <label class="ecjia-label" for="user_confirm_password">{t domain="installer"}确认密码{/t}</label>
+	                            <input type="password" name="user_confirm_password" id="user_confirm_password" class="text" value="" />
+	                            <p class="confirm-password-error"></p>
                             </li>
                             
                             <li class="control-group">
-	                            <label class="ecjia-label" for="usermail">{t domain="installer"}电子邮箱{/t}</label>
-	                            <input type="text" name="usermail" id="usermail" class="text" value="" />
+	                            <label class="ecjia-label" for="user_mail">{t domain="installer"}电子邮箱{/t}</label>
+	                            <input type="text" name="user_mail" id="user_mail" class="text" value="" />
 	                            <p class="description">{t domain="installer"}请填写一个您的常用邮箱{/t}</p>
                             </li>
                         </ul>
