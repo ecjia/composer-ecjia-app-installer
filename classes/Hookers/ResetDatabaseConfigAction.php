63f6c8bb152168b35f6aa02899902ff82e09f2f2
王正东 <wangzhengdong@ecmoban.com>
2020-12-28 16:46:07 +0800

    优化代码

--- classes/Hookers/ResetDatabaseConfigAction.php
+++ classes/Hookers/ResetDatabaseConfigAction.php
@@ -5,10 +5,6 @@ namespace Ecjia\App\Installer\Hookers;
 
 
 use Ecjia\App\Installer\DatabaseConfig;
-use ecjia_config;
-use ecjia_error;
-use RC_Time;
-use Royalcms\Component\Database\QueryException;
 
 /**
  * 重设数据库连接池
@@ -25,12 +21,12 @@ class ResetDatabaseConfigAction
      */
     public function handle()
     {
-        $db_host     = env('DB_HOST');
+        $db_host     = env('DB_HOST', '127.0.0.1');
         $db_port     = env('DB_PORT', 3306);
-        $db_user     = env('DB_USERNAME');
+        $db_user     = env('DB_USERNAME', 'root');
         $db_pass     = env('DB_PASSWORD');
         $db_database = env('DB_DATABASE');
-        $db_prefix   = env('DB_PREFIX');
+        $db_prefix   = env('DB_PREFIX', 'ecjia_');
 
         //重设数据库连接
         (new DatabaseConfig('default'))->resetConfig($db_host, $db_port, $db_user, $db_pass, $db_database, $db_prefix);
